aa700b94a44de6eb4ee9c642d1ea2816292556f0
wny24 <wny24@mails.tsinghua.edu.cn>
2026-08-17 13:43:19 +0800

    fix:  the data loader

--- robot_clip/data_loading/dataset.py
+++ robot_clip/data_loading/dataset.py
@@ -14,19 +14,25 @@ MODALITY_MAPPING = {
 def adapt_data_keys(data, modality_mapping=MODALITY_MAPPING):
     return {modality_mapping[key]: data[key] for key in data.keys()}
 
+def _match_stats(stat, tensor):
+    """Cast mean/std to the same type and device as the data array."""
+    if isinstance(tensor, torch.Tensor):
+        return torch.as_tensor(stat, dtype=tensor.dtype, device=tensor.device)
+    return np.asarray(stat, dtype=np.asarray(tensor).dtype)
+
 def normalize_data(data, normalization_params):
     normalized_data = {}
     for key, tensor in data.items():
-        mean = torch.tensor(normalization_params[key]['mean']).to(tensor.device)
-        std = torch.tensor(normalization_params[key]['std']).to(tensor.device)
+        mean = _match_stats(normalization_params[key]['mean'], tensor)
+        std = _match_stats(normalization_params[key]['std'], tensor)
         normalized_data[key] = (tensor - mean) / (std + 1e-8)
     return normalized_data
 
 def denormalize_data(data, normalization_params):
     denormalized_data = {}
     for key, tensor in data.items():
-        mean = torch.tensor(normalization_params[key]['mean']).to(tensor.device)
-        std = torch.tensor(normalization_params[key]['std']).to(tensor.device)
+        mean = _match_stats(normalization_params[key]['mean'], tensor)
+        std = _match_stats(normalization_params[key]['std'], tensor)
         denormalized_data[key] = tensor * std + mean
     return denormalized_data
 
@@ -35,16 +41,18 @@ class RobotClipDataset(Dataset):
         self.data = np.load(config.data.source_file, allow_pickle=True).item()
         self.modality_mapping = MODALITY_MAPPING
         self.data = adapt_data_keys(self.data, self.modality_mapping)
+        self.data = {k: torch.as_tensor(v, dtype=torch.float32) for k, v in self.data.items()}
         self.normalization_params = self.compute_normalization_params(self.data)
         self.normalized_data = normalize_data(self.data, self.normalization_params)
     
     def compute_normalization_params(self, data):
         normalization_params = {}
         for key, tensor in data.items():
-            mean = np.mean(tensor, axis=0)
-            std = np.std(tensor, axis=0)
-            mins = np.min(tensor, axis=0)
-            maxs = np.max(tensor, axis=0)
+            arr = tensor.detach().cpu().numpy() if isinstance(tensor, torch.Tensor) else np.asarray(tensor)
+            mean = np.mean(arr, axis=0)
+            std = np.std(arr, axis=0)
+            mins = np.min(arr, axis=0)
+            maxs = np.max(arr, axis=0)
             normalization_params[key] = {'mean': mean, 'std': std, 'mins': mins, 'maxs': maxs}
 
         return normalization_params
